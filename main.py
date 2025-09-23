9723598fbfc433cbeaf9ec00e29a61fdde546976
Polina Tsareva <tsarevapln@gmai.com>
2025-09-23 16:34:08 +0300

    STAGE_2 build: rename some function (made named shorter) decompose main func to new func - run_mode add execute of command (also with stubs)

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import shlex
 
-def parse_command(line):
+
+def prs_cmd(line):
     try:
         return shlex.split(line)
     except ValueError as e:
         print(f"Ошибка парсинга: {e}")
         return []
 
-def main():
-    vfs_name = "my_vfs"
+def execute_cmd(cmd, args):
+    """Выполняет команду и выводит результат"""
+    if cmd == "exit":
+        return True   # exit
+    elif cmd in ["ls", "cd"]:
+        print(f"[Заглушка] {cmd}: {args}")
+    else:
+        print(f"Неизвестная команда: {cmd}")
+    return False
+
+
+def run_mode(vfs_name):
+    """Интерактивный режим работы консольного приложения"""
+    print(f"Введите 'exit' для выхода.")
 
     while True:
-
         try:
             line = input(f"{vfs_name}$ ").strip()
             if not line:
                 continue
 
-            args = parse_command(line)
-            if not args:
+            parsed = prs_cmd(line)
+            if not parsed:
                 continue
 
-            cmd = args[0]
-            if cmd == "exit":
-                print("Выход из эмулятора.")
+            cmd, args = parsed[0], parsed[1:]
+            if execute_cmd(cmd, args):
                 break
-            elif cmd == "ls":
-                print(f"[Заглушка] ls: {args[1:]}")
-            elif cmd == "cd":
-                print(f"[Заглушка] cd: {args[1:]}")
-            else:
-                print(f"Неизвестная команда: {cmd}")
+
         except KeyboardInterrupt:
-            print("\nВыход...")
+            print("\nПолучен сигнал завершения (Ctrl+C). Завершаем...")
             break
+
+def main():
+    vfs_name = "my_vfs"
+
 
 if __name__ == "__main__":
     main()
