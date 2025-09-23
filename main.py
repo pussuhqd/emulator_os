6cefaf61b07d69f8daa2f6aefd04573126ba40dc
Polina Tsareva <tsarevapln@gmai.com>
2025-09-23 16:06:58 +0300

    STAGE_1 build: add line parsing into commands and args the program's basis with stubs for commands (ls,cd and exit) and error handling add text of testing program - file named scripts/test_1.bash (new dir - scripts)

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+import shlex
+
+def parse_command(line):
+    try:
+        return shlex.split(line)
+    except ValueError as e:
+        print(f"Ошибка парсинга: {e}")
+        return []
+
 def main():
     vfs_name = "my_vfs"
 
     while True:
+
         try:
-            command = input(f"{vfs_name}$ ").strip()
-            if not command:
+            line = input(f"{vfs_name}$ ").strip()
+            if not line:
                 continue
-            print(f"[DEBUG] Вы ввели: {command}")
 
+            args = parse_command(line)
+            if not args:
+                continue
 
+            cmd = args[0]
+            if cmd == "exit":
+                print("Выход из эмулятора.")
+                break
+            elif cmd == "ls":
+                print(f"[Заглушка] ls: {args[1:]}")
+            elif cmd == "cd":
+                print(f"[Заглушка] cd: {args[1:]}")
+            else:
+                print(f"Неизвестная команда: {cmd}")
         except KeyboardInterrupt:
             print("\nВыход...")
             break
